d2ca84a11581d8e705bad1fae4e7e512e965695a
Amin Lahbib <amine.lhb00@gmail.com>
2025-10-21 16:27:44 +0200

    Clerk init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { SignUpButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <Button>Click Me</Button>;
+  return <div>
+    <h1>Home Page</h1>
+    <SignUpButton>Sign Up</SignUpButton>
+  </div>
 }
